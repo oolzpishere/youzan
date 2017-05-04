963026853c6758b5d4ea3d48f75c9e985b0981f5
leeli <leeli@leelis-iMac.local>
2017-05-05 00:14:26 +0800

    todo success mail

--- mail/send_mail.rb
+++ mail/send_mail.rb
@@ -3,11 +3,13 @@ require 'pry'
 require 'json'
 
 #change root path to appropriate working directory
-root = File.expand_path("~/nut/sandbox/youzan")
-`su -l ubuntu; source ~/.zshrc`
+root = File.expand_path("~/youzan")
+`source ~/.zshrc`
 `cd #{root}; rspec spec/skus_arr_spec.rb --format j -o skus_spec_output` 
 
-path = File.expand_path("../../skus_spec_output", __FILE__ )
+error_path = File.expand_path("../../skus_spec_output", __FILE__ )
+success_path = File.expand_path("../../skus_spec_output", __FILE__ )
+
 # binding.pry
 f = File.new( path, "r")
 @text = String.new
@@ -29,7 +31,14 @@ mail = Mail.new do
   from 'page_lee@qq.com'
   to 'lizhipeng@dbtrip.com.cn'
   subject 'skus_arr_spec failed'
-  body File.read(path)
+  body File.read(error_path)
+end
+
+success_mail = Mail.new do
+  from 'page_lee@qq.com'
+  to 'lizhipeng@dbtrip.com.cn'
+  subject 'skus success report'
+  body File.read(success_path)
 end
 
 if rspec_result["summary"]["failure_count"].zero?
