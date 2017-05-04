2f02a6834a07dbbc8c759de9becc525974c03854
leeli <leeli@leelis-iMac.local>
2017-05-04 17:01:23 +0800

    add send mail

--- mail/send_mail.rb
+++ mail/send_mail.rb
@@ -1,5 +1,18 @@
 require 'mail'
 require 'pry'
+require 'json'
+
+#change root path to appropriate working directory
+root = File.expand_path("~/nut/sandbox/youzan")
+
+`cd #{root}; rspec spec/skus_arr_spec.rb --format j -o skus_spec_output` 
+
+path = File.expand_path("../../skus_spec_output", __FILE__ )
+# binding.pry
+f = File.new( path, "r")
+@text = String.new
+f.each_line { |line| @text << line}
+rspec_result = JSON.parse(@text)
 
 Mail.defaults do
   delivery_method :smtp, {
@@ -15,8 +28,14 @@ end
 mail = Mail.new do
   from 'page_lee@qq.com'
   to 'lizhipeng@dbtrip.com.cn'
-  subject 'Test ruby mail'
-  body 'Test body'
+  subject 'skus_arr_spec failed'
+  body File.read(path)
 end
-binding.pry
-mail.to_s
+
+if rspec_result["summary"]["failure_count"].zero?
+  `cd #{root}; rspec spec/job_spec.rb`
+else
+  mail.deliver 
+end
+
+
---
+++ sku_update_cron.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+rspec spec/skus_arr_spec.rb --format j -o skus_spec_output
+
